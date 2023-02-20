7403533db607db6753c1eb94c8a4ee107e046839
cheelingjet <lchee@purdue.edu>
2023-02-20 16:23:09 -0500

    Add profile and personal info sections to followup

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+//import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import ComponentTest from "../components/componentTest";
 import Example from "../components/componentTest";
+import SignUp from "./account/signup";
+import FollowUp from "./account/followup";
 
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Example></Example>;
+  return <FollowUp></FollowUp>;
 }
