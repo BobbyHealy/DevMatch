c81168d5da395f46b6294694c0aa98078f7de62d
robertohealy <healybobby@gmail.com>
2023-02-19 21:00:50 -0500

    Create login page

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -2,8 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import ComponentTest from "./componentTest";
-import Example from "./componentTest";
+import ComponentTest from "../components/componentTest";
+import Example from "../components/componentTest";
 
 const inter = Inter({ subsets: ["latin"] });
 
