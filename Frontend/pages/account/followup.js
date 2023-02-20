4170198eac24c7873a19645b2d25e9800fbe0d85
cheelingjet <lchee@purdue.edu>
2023-02-20 16:21:35 -0500

    Create temporary followup UI

---
+++ Frontend/pages/account/followup.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function FollowUp() {
+    return (
+        <div>
+            <Head>
+                <title> DevMatch</title>
+            </Head>
+
+            <h1>
+                This is a test
+            </h1>
+        </div>
+    )
+}
