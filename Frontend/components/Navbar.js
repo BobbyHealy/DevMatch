ff198add0e68b2e2a43be4daef1ade9b3f07e209
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-02-28 14:42:26 -0500

    can go back to feed

--- Frontend/components/Navbar.js
+++ Frontend/components/Navbar.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import Router from "next/router";
+
+
+const redirecttoFeed= () => {
+    Router.push('/feed');
+}
 
 export default function Navbar() {
   return (
@@ -7,9 +13,8 @@ export default function Navbar() {
         <div className='flex h-12 p-2 gap-2   items-center gap-2' > 
             <img src ="" className='bg-white h-6 w-6 rounded-full'></img>
             <span> Name</span>
-            <button className=' rounded-md border bg-white text-black my-1 py-1 px-4 text-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'> exit</button>
+            <button className=' rounded-md border bg-white text-black my-1 py-1 px-4 text-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2' onClick={()=> redirecttoFeed()}> exit</button>
         </div>
-
     </div>
   )
 }
