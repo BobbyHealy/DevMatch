9e0333c0bd90b38ba5f437561033462e5b9a34b5
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-01 14:20:37 -0500

    remove exit button on DM

--- Frontend/components/Navbar.js
+++ Frontend/components/Navbar.js
@@ -7,10 +7,6 @@ const user = {
     "https://media.licdn.com/dms/image/C4D03AQHHZKUrMMhCsQ/profile-displayphoto-shrink_800_800/0/1610704750210?e=2147483647&v=beta&t=OHuErweO0MQ3CeXJlSKkBpu-FOxPQh1sjcuVOQVTZb8",
 }
 
-const redirectToProfile= () => {
-    Router.push('./profile');
-}
-
 export default function Navbar() {
   return (
     <div className='flex items-center h-12 p-2 justify-between bg-indigo-400 text-white'>
@@ -18,7 +14,7 @@ export default function Navbar() {
         <div className='flex h-12 p-2 gap-2  items-center gap-2' > 
             <img src ={user.imageUrl} className='bg-white h-6 w-6 rounded-full'></img>
             <span className='text-sm'> {user.name}</span>
-            <button className=' rounded-md border bg-white text-black my-1 py-1 px-4 text-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2' onClick={()=> redirectToProfile()}> exit</button>
+           
         </div>
     </div>
   )
