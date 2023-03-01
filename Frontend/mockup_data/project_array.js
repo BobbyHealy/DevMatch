b082a87ac1a651a15b101ec3eed224e3a8c9ebd6
robertohealy <healybobby@gmail.com>
2023-03-01 18:07:23 -0500

    Seprated Components, BE Template, Styled Header

---
+++ Frontend/mockup_data/project_array.js
@@ -0,0 +1,46 @@
+export const projectExamplesArray = [
+  {
+    pid: "902130923821",
+    name: "The Ultimate Project",
+    owners: "1029292",
+    members: ["1212121", "423432423243", "42343243423"],
+    skills: ["JavaScript", "HTML"],
+    profile_picture:
+      "https://i.pinimg.com/474x/97/aa/84/97aa847d061a14894178805f1d551500.jpg",
+    banner_picture:
+      "https://t4.ftcdn.net/jpg/04/95/28/65/360_F_495286577_rpsT2Shmr6g81hOhGXALhxWOfx1vOQBa.jpg",
+  },
+  {
+    pid: "301923398163861",
+    name: "CS 307 Project",
+    owners: "Jeff Joceb",
+    members: ["MemberID1", "MemberID3"],
+    skills: ["C++", "C"],
+    profile_picture:
+      "https://marketplace.canva.com/EAFEits4-uw/1/0/1600w/canva-boy-cartoon-gamer-animated-twitch-profile-photo-oEqs2yqaL8s.jpg",
+    banner_picture:
+      "https://cdn.pixabay.com/photo/2015/11/19/08/52/banner-1050629__340.jpg",
+  },
+  {
+    pid: "2197639621039",
+    name: "Snab Chap",
+    owners: "1029292",
+    members: ["MemberID1", "MemberID2", "MemberID3"],
+    skills: ["React", "Node.js"],
+    profile_picture:
+      "https://i.pinimg.com/736x/17/57/1c/17571cdf635b8156272109eaa9cb5900.jpg",
+    banner_picture:
+      "https://cdn.pixabay.com/photo/2015/12/06/09/15/maple-1079235__340.jpg",
+  },
+  {
+    pid: "873129732392739",
+    name: "FakeBook",
+    owners: "1029292",
+    members: ["MemberID1", "MemberID2", "MemberID3"],
+    skills: ["JavaScript", "HTML"],
+    profile_picture:
+      "https://i.pinimg.com/474x/97/aa/84/97aa847d061a14894178805f1d551500.jpg",
+    banner_picture:
+      "https://img.freepik.com/free-vector/black-banner-with-yellow-geometric-shapes_1017-32327.jpg",
+  },
+];
--- Frontend/pages/feed.js
+++ Frontend/pages/feed.js
@@ -6,6 +6,8 @@ import UserComponent from "@/components/UserComponent";
 import ProjComponent from "@/components/ProjectComponent";
 import Router from "next/router";
 import ProjectDashBoard from "@/components/projectDashBoard";
+import Header from "@/components/header";
+import { projectExamplesArray } from "@/mockup_data/project_array";
 
 const user = {
   name: "Chelsea Hagon",
@@ -32,181 +34,9 @@ function classNames(...classes) {
 export default function Feed() {
   return (
     <div className='min-h-screen bg-gray-100'>
-      <Popover
-        as='header'
-        className={({ open }) =>
-          classNames(
-            open ? "fixed inset-0 z-40 overflow-y-auto" : "",
-            "bg-white shadow-sm lg:static lg:overflow-y-visible"
-          )
-        }
-      >
-        {({ open }) => (
-          <>
-            <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-              <div className='relative flex justify-between lg:gap-8 xl:grid xl:grid-cols-12'>
-                <div className='flex md:absolute md:inset-y-0 md:left-0 lg:static xl:col-span-2'>
-                  <div className='flex flex-shrink-0 items-center'>
-                    {/* Logo */}
-                    <h2 className='text-3xl font-bold tracking-tight text-orange-400 xl:-ml-24'>
-                      DevMatch
-                    </h2>
-                  </div>
-                </div>
-                <div className='min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6'>
-                  <div className='flex items-center px-6 py-4 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0'>
-                    <div className='w-full'>
-                      <label htmlFor='search' className='sr-only'>
-                        Search
-                      </label>
-                      <div className='relative'>
-                        <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
-                          <MagnifyingGlassIcon
-                            className='h-5 w-5 text-gray-400'
-                            aria-hidden='true'
-                          />
-                        </div>
-                        <input
-                          id='search'
-                          name='search'
-                          className='block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:border-indigo-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm'
-                          placeholder='Search'
-                          type='search'
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className='flex items-center md:absolute md:inset-y-0 md:right-0 lg:hidden'>
-                  {/* Mobile menu button */}
-                  <Popover.Button className='-mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
-                    <span className='sr-only'>Open menu</span>
-                    {open ? (
-                      <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
-                    ) : (
-                      <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
-                    )}
-                  </Popover.Button>
-                </div>
-                <div className='hidden lg:flex lg:items-center lg:justify-end xl:col-span-4'>
-                  <a
-                    href='#'
-                    className='ml-5 flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                  >
-                    <span className='sr-only'>View notifications</span>
-                    <BellIcon className='h-6 w-6' aria-hidden='true' />
-                  </a>
-
-                  {/* Profile dropdown */}
-                  <Menu as='div' className='relative ml-5 flex-shrink-0'>
-                    <div>
-                      <Menu.Button className='flex rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
-                        <span className='sr-only'>Open user menu</span>
-                        <img
-                          className='h-8 w-8 rounded-full'
-                          src={user.imageUrl}
-                          alt=''
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter='transition ease-out duration-100'
-                      enterFrom='transform opacity-0 scale-95'
-                      enterTo='transform opacity-100 scale-100'
-                      leave='transition ease-in duration-75'
-                      leaveFrom='transform opacity-100 scale-100'
-                      leaveTo='transform opacity-0 scale-95'
-                    >
-                      <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  active ? "bg-gray-100" : "",
-                                  "block py-2 px-4 text-sm text-gray-700"
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-
-                  <a
-                    href='#'
-                    className='ml-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                  >
-                    New Project
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <Popover.Panel as='nav' className='lg:hidden' aria-label='Global'>
-              <div className='mx-auto max-w-3xl space-y-1 px-2 pt-2 pb-3 sm:px-4'>
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-100 text-gray-900"
-                        : "hover:bg-gray-50",
-                      "block rounded-md py-2 px-3 text-base font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className='border-t border-gray-200 pt-4 pb-3'>
-                <div className='mx-auto flex max-w-3xl items-center px-4 sm:px-6'>
-                  <div className='flex-shrink-0'>
-                    <img
-                      className='h-10 w-10 rounded-full'
-                      src={user.imageUrl}
-                      alt=''
-                    />
-                  </div>
-                  <div className='ml-3'>
-                    <div className='text-base font-medium text-gray-800'>
-                      {user.name}
-                    </div>
-                    <div className='text-sm font-medium text-gray-500'>
-                      {user.email}
-                    </div>
-                  </div>
-                  <button
-                    type='button'
-                    className='ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                  >
-                    <span className='sr-only'>View notifications</span>
-                    <BellIcon className='h-6 w-6' aria-hidden='true' />
-                  </button>
-                </div>
-                <div className='mx-auto mt-3 max-w-3xl space-y-1 px-2 sm:px-4'>
-                  {userNavigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className='block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900'
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </Popover.Panel>
-          </>
-        )}
-      </Popover>
+      <div className='sticky top-0 z-30'>
+        <Header />
+      </div>
 
       <div className='py-6'>
         <div className='mx-auto max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-8 lg:px-8'>
@@ -215,35 +45,27 @@ export default function Feed() {
               aria-label='Sidebar'
               className='sticky top-6 divide-y divide-gray-300'
             >
-              
               {/* Left */}
             </nav>
           </div>
           <main className='lg:col-span-9 xl:col-span-6'>
             {/* Update this to use API + map to generate a bunch of enties of either User or Project components */}
             {/* ProjComponent on feed example */}
-            <div className='pb-4'>
-              <ProjComponent />
-            </div>
-            <div className='pb-4'>
-              <ProjComponent />
-            </div>
-            <div className='pb-4'>
-              <ProjComponent />
-            </div>
-            <div className='pb-4'>
-              <ProjComponent />
-            </div>
-            <div className='pb-4'>
-              <ProjComponent />
-            </div>
-            {/* User Component on feed example */}
-            <div className='pb-4'>
-              <UserComponent />
-            </div>
+            {projectExamplesArray.map((e) => {
+              return (
+                <div className='pb-6'>
+                  <ProjComponent project={e} />
+                </div>
+              );
+            })}
           </main>
           <aside className='hidden xl:col-span-4 xl:block'>
-            <div className='sticky top-6 space-y-4 h-full'>{/* Right */}<ProjectDashBoard/></div>
+            <div className='sticky top-6 space-y-4 h-full'>
+              {/* Right */}
+              <div className='sticky top-28 z-30'>
+                <ProjectDashBoard />
+              </div>
+            </div>
           </aside>
         </div>
       </div>
