619b0ac6c499762ed4e24b6b3c178874a58d8b34
cheelingjet <lchee@purdue.edu>
2023-03-03 02:23:41 -0500

    Completed ProjectModal component for proj previews

---
+++ Frontend/components/ProjectModal.js
@@ -0,0 +1,20 @@
+import React, { Children } from "react";
+
+const ProjectModal = ({ isVisible, onClose, children }) => {
+    if (!isVisible) return null;
+
+const handleClose = (e) => {
+    if( e.target.id === 'wrapper' ) onClose();
+}
+
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center" 
+        id="wrapper" onClick={handleClose}>
+            <div className="w-[600px] flex flex-col">
+                <div className="bg-white p-2 rounded">{children}</div>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectModal;
