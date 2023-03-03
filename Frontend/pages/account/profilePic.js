1e0d8e9c372062a6ee77f01759fd4dc952cdcc92
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-03 14:13:40 -0500

    make It the same as acceptance criteria

--- Frontend/pages/account/profilePic.js
+++ Frontend/pages/account/profilePic.js
@@ -92,16 +92,16 @@ function profilePic() {
                 src={url} alt=""/>}
                 </div>
                 <div className="upload text-center p-4">
-                <InputText type = "file" 
+                {!image&&<InputText type = "file" 
                     accept="*.jpg"
                     onChange={(event)=>{
                     const file = event.target.files[0];
                     setimage(file)
                     }}
-                    />
-                    <button className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    />}
+                    {image&&<button className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     onClick={handleSubmit}
-                    >Upload</button>
+                    >Save</button>}
                     <button className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     onClick={()=> redirectToProfile()}
                     >Cancel</button>
