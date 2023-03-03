e47fb422cb0e5a10f20b81742eae7ad3e08b89d0
robertohealy <healybobby@gmail.com>
2023-03-02 23:27:19 -0500

    Make projects editable

--- Frontend/components/UserComponent.js
+++ Frontend/components/UserComponent.js
@@ -30,7 +30,11 @@ export default function UserComponent(props) {
         <div className='flex-shrink-0'>
           <img
             className='h-10 w-10 rounded-full'
-            src='https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+            src={
+              user.profilePic !== undefined
+                ? user.profilePic
+                : "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            }
             alt=''
           />
         </div>
