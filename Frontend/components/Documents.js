5f16b8983e84add93bcdfe32c91d4a2883f2842f
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-21 22:37:39 -0400

    Documents UI and backend done

--- Frontend/components/Documents.js
+++ Frontend/components/Documents.js
@@ -2,6 +2,7 @@
 import React,{use, useState,useEffect}from 'react'
 import { db } from '@/config/firebase';
 import { useAuth } from '@/context/AuthContext';
+import DocumentRow from './DocumentRow';
 // import {useCollection,useCollectionDataOnce} from "react-firebase-hooks/firestore";
 import {
     onSnapshot,
@@ -131,10 +132,11 @@ export default function Documents() {
                     <path d="M19.5 21a3 3 0 003-3v-4.5a3 3 0 00-3-3h-15a3 3 0 00-3 3V18a3 3 0 003 3h15zM1.5 10.146V6a3 3 0 013-3h5.379a2.25 2.25 0 011.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 013 3v1.146A4.483 4.483 0 0019.5 9h-15a4.483 4.483 0 00-3 1.146z" />
                     </svg>
                 </div>
+            
+            {documents.map((doc)=>(
+              <DocumentRow key = {doc.id} id ={doc.id} fileName ={doc.fileName} date = {doc.time}/>
+            ))}
             </div>
-            {/* {documents.map(doc=>(
-              
-            ))} */}
         </section>
 
     </div>
