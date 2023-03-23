46b7465f66956692d6d99e8b73fd63c0c3f9bac7
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-23 18:03:31 -0400

    Update Hover

--- Frontend/pages/project/index.js
+++ Frontend/pages/project/index.js
@@ -243,17 +243,17 @@ export default function ProjectSpace(){
                   />
                 </div>
                 <div className='ml-3 pr-10'>
-                  <p className='text-sm font-medium text-gray-400 group-hover:text-gray-200'>
+                  <p className='text-sm font-medium text-gray-400 '>
                     {userInfo.name}
                   </p>
                   <p
-                    className='text-xs font-medium text-gray-500 group-hover:text-gray-700'
+                    className='text-xs font-medium text-gray-500 hover:text-gray-700'
                     onClick={()=>RedirectTo("account")}
                   >
                     View profile
                   </p>
                 </div>
-                <div className="text-xs w-12 font-bold tracking-tight text-orange-400 ">
+                <div className="text-xs w-12 font-bold tracking-tight text-orange-400 hover:text-orange-800">
                     <span onClick={()=>RedirectTo("")}>Back To Feed</span>
                 </div>
               </div>
