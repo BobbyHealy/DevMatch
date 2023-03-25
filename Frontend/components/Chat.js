5d3f8fe3590f6affe8135bcc652ad553c95d1700
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-25 17:10:09 -0400

    Refractor code
    refractor code and make sure an user can't send a dm without receiver

--- Frontend/components/Chat.js
+++ Frontend/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import  { useEffect, useState} from "react";
 import Messages from './Messages'
 import ChatInput from './ChatInput'
 import {
@@ -12,13 +12,14 @@ import { useAuth } from "@/context/AuthContext";
 export default function Chat() {
   const { user, userInfo} = useAuth();
   const [receiver, setReceiver] = useState(null);
+  const [DMID, setDMID] = useState()
 
   useEffect( () => {
     const getChat = () => {
       const unsub = onSnapshot(doc(db, "users", user.uid), (doc) => {
         if(doc.data().currentChat)
         {
-          setReceiver(doc.data().currentChat.displayName);
+          setReceiver(doc.data().currentChat);
         }
      
       });
@@ -29,19 +30,28 @@ export default function Chat() {
     };
     user.uid && getChat()
   }, [user.uid]);
+  useEffect(() => {
+    if (receiver)
+    {
+      const dmID =  user.uid > receiver.uid
+      ? user.uid + receiver.uid
+      : receiver.uid + user.uid;
+    setDMID(dmID);
+    }
+  }, [receiver]);
   return (
     
     <div className='flex-2 basis-3/4 bg-orange-300 '>
         <div className=' h-12 bg-orange-700 flex items-center justify-between p-2  gap-2'>
           {!receiver&&<span>Receiver</span>}
-          {receiver&&<span>{receiver}</span>}
+          {receiver&&<span>{receiver.displayName}</span>}
             <div className='flex gap-2'>
                 <img className='h-6 cursor-pointer'></img>
             </div>
 
         </div>
-        <Messages/>
-        <ChatInput/>
+        {<Messages DMID={DMID}/>}
+        <ChatInput DMID={DMID} receiver ={receiver}/>
 
     </div>
   )
