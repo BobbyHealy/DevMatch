7f4eb4429e42cd91a8d56c4c52dd8f7b8c8343fb
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-27 15:19:17 -0400

    added scroll for chat list and redesign the UI
    - Allow user to scroll through chat list when it exceed the window
    - Redesign the UI
    
    Problem:
    - when search result appear the scroll the chat list will be pushed down and can't scroll through all the result within window.

--- Frontend/components/Searchbar.js
+++ Frontend/components/Searchbar.js
@@ -14,7 +14,7 @@ import {
 
 
 
-export default function Searchbar() {
+export default function Searchbar({search}) {
 
     const {user, userInfo} = useAuth()
     const currentUser = {
@@ -22,11 +22,23 @@ export default function Searchbar() {
         name: userInfo.name,
         photoURL: userInfo.profilePic, 
     }
+
+  
     const [username, setUsername] = useState("")
-    const [snap,setSnap] = useState()
 
     const [users, setUsers] = useState([])
+    const [result, setResult] = useState(false)
     const [err, setErr] = useState(false)
+    useEffect(() => {
+      if(!search)
+      {
+        setUsername("")
+        setUsers([])
+        console.log("no search")
+        setResult(false)
+      }
+
+    }, [search]);
     const handleSearch = async () => {
         const q = query(
           collection(db, "users")
@@ -45,46 +57,58 @@ export default function Searchbar() {
           console.log(result)
           console.log(users)
         } catch (err) {
-          console.log(err)
           setErr(true);
         }
       };
     const handleOnclick =()=>
     {
       setUsers([])
+      console.log("set false")
+      setResult(false)
     }
-    const handleKey = e=>{
+    const handleKey = async e=>{
         // setUser(null)
 
-        if(username)
+        if(username.trim())
         {
-          e.code ==="Enter" &&handleSearch()&&setUsername("")
+          console.log("set true")
+          e.code ==="Enter" &&await handleSearch()
+          setUsername("")
+      
+          setResult(true)
+     
      
         }
         else
         {
-          setUsers([])&&setUsername("")
+          console.log("clear")
+          setUsers([])
+          setUsername("")
+          setResult(false)
         }  
     }
   return (
-    <div className='search border-b-2 border-gray-400'>
+    <div className='search '>
         <div className='searchForm p-2'>
             <input type="text" 
-            className='bg-transparent border-none outline-none text-white placeholder-gray-300' 
+            className='bg-transparent rounded-lg border-none outline-none text-white placeholder-gray-300' 
             placeholder='Search for a user'
             onChange={e=>setUsername(e.target.value)}
             onKeyDown={handleKey}
             value={username}
             />
         </div>
-        {err && <span>User not found!</span>}
-        {users&&users.map((u)=> (
+        {/* {err && <span>User not found!</span>} */}
+
+        {result&&<div className="border-b border-gray-800">
+        {users.length>0?users&&users.map((u)=> (
               <div onClick={handleOnclick}>
               <SearchResult 
               key={u.uid} user2={u}/>
               {console.log(u)}
               </div>
-              ))}
+              )):<span>User not found!</span>}
+        </div>}
     </div>
   )
 }
