23b126dd26b34ecbf0c8e339aa6eed831e826192
robertohealy <healybobby@gmail.com>
2023-03-29 15:51:44 -0400

    Fixed deactivate bug

--- Frontend/components/Profile.js
+++ Frontend/components/Profile.js
@@ -1,32 +1,29 @@
 import React, { Component, useEffect, useState } from "react";
 import Router from "next/router";
 import { EnvelopeIcon } from "@heroicons/react/20/solid";
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { useAuth } from "@/context/AuthContext";
 import DeactivateModal from "./DeactivateModal";
-import {doc,updateDoc, deleteDoc} from "firebase/firestore";
+import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 import { db } from "@/config/firebase";
-import {deleteUser, getAuth} from "firebase/auth";
+import { deleteUser, getAuth } from "firebase/auth";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 function Profile() {
-
-
   const { user, login, logout, userInfo } = useAuth();
-  const currentUser ={
+  const currentUser = {
     userID: user.userID !== undefined ? userInfo.userID : "",
     name: userInfo.name !== undefined ? userInfo.name : "Foo Bar",
     email: user.email !== undefined ? user.email : "foo@bar.com",
     rating: userInfo.rating !== undefined ? userInfo.rating : 100,
-    profilePic:
-          userInfo.profilePic !== undefined ? userInfo.profilePic : "",
+    profilePic: userInfo.profilePic !== undefined ? userInfo.profilePic : "",
     pOwned: userInfo.pOwned !== undefined ? userInfo.pOwned : [],
     pJoined: userInfo.pJoined !== undefined ? userInfo.pJoined : [],
     skills: userInfo.skills !== undefined ? userInfo.skills : [],
-    description: userInfo.description !== undefined ? userInfo.description : ""
-  }
+    description: userInfo.description !== undefined ? userInfo.description : "",
+  };
   const [showModal, setShowModal] = useState(false);
   const [completeUser, setCompleteUser] = useState({
     userId: "",
@@ -37,7 +34,7 @@ function Profile() {
     pOwned: [""],
     pJoined: [""],
     skills: [""],
-    description:""
+    description: "",
   });
   const [newSkills, setNewSkills] = useState(undefined);
   const [newName, setNewName] = useState(undefined);
@@ -51,12 +48,10 @@ function Profile() {
   function refreshPage() {
     window.location.reload(false);
   }
-  useEffect(()=>
-  {
-    setPO(userInfo.pOwned)
-    setPJ(userInfo.pJoined)
-    
-  },[])
+  useEffect(() => {
+    setPO(userInfo.pOwned);
+    setPJ(userInfo.pJoined);
+  }, []);
 
   useEffect(() => {
     if (user === null && userInfo === null) {
@@ -71,37 +66,27 @@ function Profile() {
         pOwned: [],
         pJoined: [],
         skills: [],
-        description: ""
+        description: "",
       });
     } else {
       setCompleteUser(currentUser);
-      if(userInfo.skills)
-      {
-        setNewSkills(
-          userInfo.skills.join(",")
-        );
-
+      if (userInfo.skills) {
+        setNewSkills(userInfo.skills.join(","));
       }
     }
     updateDoc(doc(db, "users", user.uid), {
-      currentPage:"Overview"
-    })
+      currentPage: "Overview",
+    });
   }, [user, userInfo]);
-
-
 
   const tabs = [{ name: "Profile", href: "#", current: true }];
   const profileImageURL =
     "https://cdn.britannica.com/79/114979-050-EA390E84/ruins-St-Andrews-Castle-Scotland.jpg";
 
-
   useEffect(() => {
-
-
-    var projects =[]
-      if(pO )
-      {
-        pO.map((project)=>{
+    var projects = [];
+    if (pO) {
+      pO.map((project) => {
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         var raw = JSON.stringify({
@@ -114,68 +99,54 @@ function Profile() {
         };
         fetch("http://localhost:3000/api/getProject", requestOptions)
           .then((response) => response.text())
-          .then((result) => 
-          {
-            projects.push(JSON.parse(result))
-            if(pO.length===projects.length)
-            {
-              setProjectsO(projects)
+          .then((result) => {
+            projects.push(JSON.parse(result));
+            if (pO.length === projects.length) {
+              setProjectsO(projects);
             }
-          }
-          )
+          })
           .catch((err) => {
             console.log(err);
           });
-          
-        })
-        
-      }
-   
+      });
+    }
   }, [pO]);
 
   useEffect(() => {
-     var projects =[]
+    var projects = [];
 
-    if (pJ)
-    {
-      pJ.map((project)=>{
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      var raw = JSON.stringify({
-        pid: project,
-      });
-      var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-      };
-      fetch("http://localhost:3000/api/getProject", requestOptions)
-        .then((response) => response.text())
-        .then((result) =>  
-        {
-          projects.push(JSON.parse(result))
-          if(pJ.length===projects.length)
-          {
-            setProjectsJ(projects)
-          }
-
-        })
-        .catch((err) => {
-          console.log(err);
+    if (pJ) {
+      pJ.map((project) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+          pid: project,
         });
-    })
+        var requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+        };
+        fetch("http://localhost:3000/api/getProject", requestOptions)
+          .then((response) => response.text())
+          .then((result) => {
+            projects.push(JSON.parse(result));
+            if (pJ.length === projects.length) {
+              setProjectsJ(projects);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     }
-
-    
-   
   }, [pJ]);
   const handleEdit = () => {
     setEdit(true);
   };
   const deleteUser = async () => {
-
-    const currentUser = getAuth().currentUser
-    setShowModal(false)
+    const currentUser = getAuth().currentUser;
+    setShowModal(false);
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     var raw = JSON.stringify({
@@ -192,36 +163,39 @@ function Profile() {
       .catch((err) => {
         console.log(err);
       });
-    
-      handleDelete()
-    currentUser.delete().then(() => {
-      handleDelete()
-      Router.push('/account/login')
-      refreshPage()
-    }).catch((error) => {
 
-    });
-  }
-
-  const handleDelete = async () => {
-    await deleteDoc(doc(db, "users", user.uid))
-    refreshPage()
+    currentUser
+      .delete()
+      .then(() => {
+        handleDelete();
+        Router.push("/account/login");
+        refreshPage();
+      })
+      .catch((error) => {});
   };
 
-
+  const handleDelete = async () => {
+    await deleteDoc(doc(db, "users", user.uid));
+    refreshPage();
+  };
 
   const handleSubmit = async (e) => {
-    console.log(newName==="")
-    const skillsArr = newSkills!== undefined? newSkills.split(","): undefined
+    console.log(newName === "");
+    const skillsArr =
+      newSkills !== undefined ? newSkills.split(",") : undefined;
     var raw = JSON.stringify({
       userID: user.uid,
-      name: newName !== undefined&&newName.trim().length>0 ? newName.trim() : userInfo.name,
+      name:
+        newName !== undefined && newName.trim().length > 0
+          ? newName.trim()
+          : userInfo.name,
       rating: 100,
-      skills: skillsArr!== undefined ? skillsArr : userInfo.name,
+      skills: skillsArr !== undefined ? skillsArr : userInfo.name,
       pOwned: userInfo.pOwned !== undefined ? userInfo.pOwned : undefined,
       pJoined: userInfo.pJoined !== undefined ? userInfo.pJoined : undefined,
-      profilePic: userInfo.profilePic !== undefined ? userInfo.profilePic : undefined,
-      description: des !== undefined ? des: userInfo.description
+      profilePic:
+        userInfo.profilePic !== undefined ? userInfo.profilePic : undefined,
+      description: des !== undefined ? des : userInfo.description,
     });
 
     var myHeaders = new Headers();
@@ -233,8 +207,11 @@ function Profile() {
       body: raw,
     };
     updateDoc(doc(db, "users", user.uid), {
-      displayName: newName !== undefined&&newName.trim().length>0 ? newName.trim() : userInfo.name
-    })
+      displayName:
+        newName !== undefined && newName.trim().length > 0
+          ? newName.trim()
+          : userInfo.name,
+    });
 
     fetch("http://localhost:3000/api/addUser", requestOptions)
       .then((response) => response.text())
@@ -242,10 +219,10 @@ function Profile() {
         setEdit(false);
       })
       .catch((error) => console.log("error", error));
-      setNewName(undefined)
-      setDes(undefined)
-      setNewSkills(undefined)
-      refreshPage()
+    setNewName(undefined);
+    setDes(undefined);
+    setNewSkills(undefined);
+    refreshPage();
   };
 
   // const handleSubmit = () => {
@@ -375,36 +352,40 @@ function Profile() {
                 {"Owned Projects"}
               </dt>
               <dd className='mt-1 text-sm text-gray-900'>
-              {projectsO.length>0?projectsO.sort((a,b)=>
-              {
-                if(a.name<b.name)
-                {
-                  return -1
-                }else
-                {
-                  return 1
-                }
-              }).map((project)=>
-                        (<li key={project.pid}>{project.name}</li>)):"N/A"}
+                {projectsO.length > 0
+                  ? projectsO
+                      .sort((a, b) => {
+                        if (a.name < b.name) {
+                          return -1;
+                        } else {
+                          return 1;
+                        }
+                      })
+                      .map((project) => (
+                        <li key={project.pid}>{project.name}</li>
+                      ))
+                  : "N/A"}
               </dd>
             </div>
 
-            <div className='sm:col-span-1'>  
+            <div className='sm:col-span-1'>
               <dt className='text-sm font-medium text-gray-500'>
                 {"Joined Projects"}
               </dt>
               <dd className='mt-1 text-sm text-gray-900'>
-              {projectsJ.length>0?projectsJ.sort((a,b)=>
-              {
-                if(a.name<b.name)
-                {
-                  return -1
-                }else
-                {
-                  return 1
-                }
-              }).map((project)=>
-                        (<li key={project.pid}>{project.name}</li>)): "N/A"}
+                {projectsJ.length > 0
+                  ? projectsJ
+                      .sort((a, b) => {
+                        if (a.name < b.name) {
+                          return -1;
+                        } else {
+                          return 1;
+                        }
+                      })
+                      .map((project) => (
+                        <li key={project.pid}>{project.name}</li>
+                      ))
+                  : "N/A"}
               </dd>
             </div>
 
@@ -475,50 +456,57 @@ function Profile() {
               </dd>
             </div>
             <div className='sm:col-span-1 py-6'>
-                <button
-                  type="button"
-                  className="rounded-full bg-red-600 py-2 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                  onClick={() => setShowModal(true)}
-                >
-                  Deactivate Account
-                </button>
+              <button
+                type='button'
+                className='rounded-full bg-red-600 py-2 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'
+                onClick={() => setShowModal(true)}
+              >
+                Deactivate Account
+              </button>
             </div>
           </dl>
         )}
       </div>
-      <DeactivateModal isVisible={showModal} onClose={() => setShowModal(false)}>
-            <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-base font-semibold leading-6 text-gray-900">
-                      Deactivate account
-                    </h3>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to deactivate your account? All of your data will be permanently removed
-                        from our servers forever. This action cannot be undone.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                    onClick={deleteUser}
-                  >
-                    Deactivate
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Cancel
-                  </button>
-              </div>
+      <DeactivateModal
+        isVisible={showModal}
+        onClose={() => setShowModal(false)}
+      >
+        <div className='sm:flex sm:items-start'>
+          <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
+            <ExclamationTriangleIcon
+              className='h-6 w-6 text-red-600'
+              aria-hidden='true'
+            />
+          </div>
+          <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+            <h3 className='text-base font-semibold leading-6 text-gray-900'>
+              Deactivate account
+            </h3>
+            <div className='mt-2'>
+              <p className='text-sm text-gray-500'>
+                Are you sure you want to deactivate your account? All of your
+                data will be permanently removed from our servers forever. This
+                action cannot be undone.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='mt-5 sm:mt-4 sm:flex sm:flex-row-reverse'>
+          <button
+            type='button'
+            className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
+            onClick={deleteUser}
+          >
+            Deactivate
+          </button>
+          <button
+            type='button'
+            className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+            onClick={() => setShowModal(false)}
+          >
+            Cancel
+          </button>
+        </div>
       </DeactivateModal>
     </div>
   );
