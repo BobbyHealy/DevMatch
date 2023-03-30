b372950faa98d946747cc0a83c6117bf4ca59285
cheelingjet <lchee@purdue.edu>
2023-03-30 01:15:50 -0400

    Created scrum board UI Scrumboard layout, as well as individual task cells. Uses the react-beautiful-dnd package, for drag and drop functions Requires react-strict-mode to be set to false for drag and drop to work.

--- Frontend/components/Scrumboard.js
+++ Frontend/components/Scrumboard.js
@@ -1,10 +1,26 @@
-import React,{useEffect} from "react";
-import GC from "./GC";
 import { useAuth } from "@/context/AuthContext";
 import {doc,updateDoc,} from "firebase/firestore";
 import { db } from "@/config/firebase";
+import ScrumTask from "./ScrumTask";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import ScrumData from '../mockup_data/scrum-data.json'
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { useEffect, useState } from "react";
+
+
+function createGuidId() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
 export default function Scrumboard() {
+  const [ready, setReady] = useState(false);
+  const [boardData, setBoardData] = useState(ScrumData);
+  const [showForm, setShowForm] = useState(false);
+  const [selectedBoard, setSelectedBoard] = useState(0);
+  
   const{user}= useAuth();
   useEffect(() => {
     if(user.uid)
@@ -12,9 +28,130 @@ export default function Scrumboard() {
       updateDoc(doc(db, "users", user.uid), {
         currentProjPage:"#Scrum"
       })
+      setReady(true);
     }
-  }, [])
+  }, []);
+
+  const onDragEnd = (re) => {
+    if (!re.destination) return;
+    let newBoardData = boardData;
+    var dragItem =
+      newBoardData[parseInt(re.source.droppableId)].items[re.source.index];
+    newBoardData[parseInt(re.source.droppableId)].items.splice(
+      re.source.index,
+      1
+    );
+    newBoardData[parseInt(re.destination.droppableId)].items.splice(
+      re.destination.index,
+      0,
+      dragItem
+    );
+    setBoardData(newBoardData);
+  };
+
+  const onTextAreaKeyPress = (e) => {
+    if(e.keyCode === 13) //Enter
+    {
+      const val = e.target.value;
+      if(val.length === 0) {
+        setShowForm(false);
+      }
+      else {
+        const boardId = e.target.attributes['data-id'].value;
+        const item = {
+          id: createGuidId(),
+          title: val,
+          category: 2,
+          assignees: ["Placeholder"]
+        }
+        let newBoardData = boardData;
+        newBoardData[boardId].items.push(item);
+        setBoardData(newBoardData);
+        setShowForm(false);
+        e.target.value = '';
+      }
+    }
+  }
   return (
-    <div>scrumboard</div>
-  )
+    <div className="p-10 flex flex-col h-screen">
+      {ready && (
+          <DragDropContext onDragEnd={onDragEnd}>
+            <div className="grid grid-cols-3 gap-7 my-3">
+              {boardData.map((board, bIndex) => {
+                return (
+                  <div key={board.name}>
+                    <Droppable droppableId={bIndex.toString()}>
+                      {(provided, snapshot) => (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          <div
+                            className={`bg-gray-100 rounded-md shadow-md
+                            flex flex-col relative overflow-hidden 
+                            ${snapshot.isDraggingOver && "bg-green-100"}`}
+                          >
+                            <span
+                            className={`w-full h-1 absolute inset-x-0 top-0 bg-gradient-to-r
+                              ${
+                                board.name == "Backlog Tasks"
+                                  ? "from-red-500 to-red-200"
+                                  : board.name == "In Progress"
+                                  ? "from-yellow-500 to-yellow-200"
+                                  : "from-green-500 to-green-200"
+                              }
+                              `}
+                            ></span>
+                            <h4 className=" p-3 flex justify-between items-center mb-2">
+                              <span className="text-2xl text-gray-600">
+                                {board.name}
+                              </span>
+                            </h4>
+
+                            <div className="overflow-y-auto overflow-x-hidden h-auto"
+                            style={{maxHeight:'calc(100vh - 290px)'}}>
+                              {board.items.length > 0 &&
+                                board.items.map((item, iIndex) => {
+                                  return (
+                                    <ScrumTask
+                                      key={item.id}
+                                      data={item}
+                                      index={iIndex}
+                                      className="m-3"
+                                    />
+                                  );
+                                })}
+                              {provided.placeholder}
+                            </div>
+                            
+                            {
+                              showForm && selectedBoard === bIndex ? (
+                                <div className="p-3">
+                                  <textarea className="border-gray-300 rounded focus:ring-purple-400 w-full" 
+                                  rows={3} placeholder="Task info" 
+                                  data-id={bIndex}
+                                  onKeyDown={(e) => onTextAreaKeyPress(e)}/>
+                                </div>
+                              ): (
+                                <button
+                                  className="flex justify-center items-center my-3 space-x-2 text-lg"
+                                  onClick={() => {setSelectedBoard(bIndex); setShowForm(true);}}
+                                >
+                                  <span>Add task</span>
+                                  <PlusCircleIcon className="w-5 h-5 text-gray-500" />
+                                </button>
+                              )
+                            }
+                          </div>
+                        </div>
+                      )}
+                    </Droppable>
+                  </div>
+                );
+              })}
+            </div>
+          </DragDropContext>
+        )}
+    </div>
+  );
 }
