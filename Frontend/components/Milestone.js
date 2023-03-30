b9913bf9e8f29936a57b091e1d168852977d0bee
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-30 19:27:39 -0400

    Added user restriction to create milestone button

--- Frontend/components/Milestone.js
+++ Frontend/components/Milestone.js
@@ -184,14 +184,14 @@ useEffect(() => {
                 <h1 className="text-base font-semibold leading-6 text-gray-900">Milestones</h1>
                 </div>
                 <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                <button
+                {project.owners?.includes(user.uid)&&<button
                     type='button'
                     className='inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                     onClick={() => setShowModal(true)}
                     >
                     Create new Milestone
                     <PlusIcon className='ml-2 -mr-1 h-5 w-5' aria-hidden='true' />
-                </button>
+                </button>}
                 </div>
             </div>
             <div className="mt-8 flow-root">
