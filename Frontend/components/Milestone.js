d955fbdf070ea4da771a4351555ece0710989aa2
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-31 14:51:15 -0400

    hotfix milestone

--- Frontend/components/Milestone.js
+++ Frontend/components/Milestone.js
@@ -147,7 +147,7 @@ useEffect(() => {
         body: raw,
         };
 
-        fetch("http://localhost:3000/api/addMilestone", requestOptions)
+        await fetch("http://localhost:3000/api/addMilestone", requestOptions)
         .then((response) => response.text())
         .then((result) => {
             console.log(JSON.parse(result));
