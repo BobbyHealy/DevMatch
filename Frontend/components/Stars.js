df14c3dd902780503379d5ecb2661dbac21d8a72
cheelingjet <lchee@purdue.edu>
2023-04-16 22:25:41 -0400

    Finalized design for rating UI page

--- Frontend/components/Stars.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Star() {
-    return (
-        <div>
-            
-        </div>
-    );
-}
--- Frontend/pages/account/RatingForm.js
+++ Frontend/pages/account/RatingForm.js
@@ -1,7 +1,20 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { PhotoIcon, UserCircleIcon, StarIcon } from '@heroicons/react/24/solid'
 import Header from '@/components/header'
+import { useState } from 'react';
+
 
 export default function Example() {
+    const [techSkill, settechValue] = useState(0);
+    const [commSkill, setcommValue] = useState(0);
+    const [leadSkill, setleadValue] = useState(0);
+	const [isSubmit, setIsSubmit] = useState(false);
+
+	const handleSubmit = () => {
+		if (rateValue) {
+			setIsSubmit(true);
+		}
+	};
+
   return (
     <form>
       <div className="space-x-6 space-y-12 bg-gray-100">
@@ -18,35 +31,93 @@ export default function Example() {
             
 
             <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                About
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
-              </div>
-              <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+                <div className="w-150 h-72 p-5 rounded-3xl text-white flex flex-col gap-8 bg-gray-500">
+                    <div>
+                        <StarIcon className="w-10 h-10 text-orange-500"/>
+                    </div>
+                    <h1 className="text-2xl font-bold">Technical Skill</h1>
+                    <p className="text-sm">
+                        Provide a rating based on the user's proficiency in their skills
+                    </p>
+                    <div className="grid grid-cols-5 gap-5">
+                        {[1, 2, 3, 4, 5].map((value) => {
+                            return (
+                                <div
+                                    key={value}
+                                    className={`grid place-content-center  h-12 w-12 rounded-full cursor-pointer  transition-all ${
+                                        value === techSkill
+                                            ? "bg-orange-500  text-white"
+                                            : "text-gray-400 hover:bg-white hover:text-orange-500  bg-zinc-900"
+                                    }`}
+                                    onClick={() => settechValue(value)}
+                                >
+                                    {value}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
-                Photo
-              </label>
-              <div className="mt-2 flex items-center gap-x-3">
-                <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
-                <button
-                  type="button"
-                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  Change
-                </button>
-              </div>
+                <div className="w-150 h-72 p-5 rounded-3xl text-white flex flex-col gap-8 bg-gray-500">
+                    <div>
+                        <StarIcon className="w-10 h-10 text-orange-500"/>
+                    </div>
+                    <h1 className="text-2xl font-bold">Communication</h1>
+                    <p className="text-sm">
+                        Provide a rating based on the user's communication skills
+                    </p>
+                    <div className="grid grid-cols-5 gap-5">
+                        {[1, 2, 3, 4, 5].map((value) => {
+                            return (
+                                <div
+                                    key={value}
+                                    className={`grid place-content-center  h-12 w-12 rounded-full cursor-pointer  transition-all ${
+                                        value === commSkill
+                                            ? "bg-orange-500  text-white"
+                                            : "text-gray-400 hover:bg-white hover:text-orange-500  bg-zinc-900"
+                                    }`}
+                                    onClick={() => setcommValue(value)}
+                                >
+                                    {value}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
+
+            <div className="col-span-full">
+                <div className="w-150 h-72 p-5 rounded-3xl text-white flex flex-col gap-8 bg-gray-500">
+                    <div>
+                        <StarIcon className="w-10 h-10 text-orange-500"/>
+                    </div>
+                    <h1 className="text-2xl font-bold">Leadership</h1>
+                    <p className="text-sm">
+                        Provide a rating based on the user's leadership qualities
+                    </p>
+                    <div className="grid grid-cols-5 gap-5">
+                        {[1, 2, 3, 4, 5].map((value) => {
+                            return (
+                                <div
+                                    key={value}
+                                    className={`grid place-content-center  h-12 w-12 rounded-full cursor-pointer  transition-all ${
+                                        value === leadSkill
+                                            ? "bg-orange-500  text-white"
+                                            : "text-gray-400 hover:bg-white hover:text-orange-500  bg-zinc-900"
+                                    }`}
+                                    onClick={() => setleadValue(value)}
+                                >
+                                    {value}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+
+
           </div>
         </div>
       </div>
@@ -55,11 +126,11 @@ export default function Example() {
                 Cancel
             </button>
             <button
-            type="submit"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-                Save
-            </button>
+				className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+				onClick={handleSubmit}
+			>
+				Submit
+			</button>
         </div>
     </form>
   )
