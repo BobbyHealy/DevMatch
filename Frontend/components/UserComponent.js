f8e0c7288f1d34e75657d31743f3e64826754859
Jeff Chen <jch00078@gmail.com>
2023-04-18 17:22:39 -0400

    another typo fix

--- Frontend/components/UserComponent.js
+++ Frontend/components/UserComponent.js
@@ -275,7 +275,7 @@ export default function UserComponent(props) {
                 htmlFor='email'
                 className='block text-sm font-medium leading-6 text-gray-900'
               >
-                Confirm by writting the user's name
+                Confirm by writing the user's name
               </label>
               <div className='mt-2'>
                 <input
