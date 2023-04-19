bbd133d62d1dcb306d5ff58c86cfcef07ce3090d
cheelingjet <lchee@purdue.edu>
2023-04-18 20:24:06 -0400

    Added warning message modal and text box for user to provide reason for reporting

--- Frontend/components/UserComponent.js
+++ Frontend/components/UserComponent.js
@@ -41,6 +41,7 @@ const default_user = {
 
 export default function UserComponent(props) {
   const [showModal, setShowModal] = useState(false);
+  const [showReportModal, setReportModal] = useState(false);
 
   const {
     user = default_user,
@@ -241,6 +242,7 @@ export default function UserComponent(props) {
             <></>
           )}
           <button
+            onClick={() => setReportModal(true)}
             type='button'
             className='inline-flex items-center rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2'
           >
@@ -300,6 +302,59 @@ export default function UserComponent(props) {
             type='button'
             className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
             onClick={() => setShowModal(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </DeactivateModal>
+
+      <DeactivateModal
+        isVisible={showReportModal}
+        onClose={() => setReportModal(false)}
+      >
+        <div>
+          <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
+            <FlagIcon
+              className='h-6 w-6 text-red-600'
+              aria-hidden='true'
+            />
+          </div>
+          <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+            <h3 className='text-base font-semibold leading-6 text-gray-900'>
+              Report User
+            </h3>
+            <div className='mt-2'>
+              <p className='text-sm text-gray-500'>
+                Provide a reason for reporting this user:
+              </p>
+            </div>
+
+            <div className='mt-2'>
+              <div className='mt-2'>
+                <textarea
+                  id="report"
+                  name="report"
+                  rows={4}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-5 sm:mt-4 space-x-5 sm:flex sm:flex-row-reverse'>
+        <button
+            type='submit'
+            className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto'
+            onClick={() => setReportModal(false)}
+          >
+            Submit
+          </button>
+          <button
+            type='button'
+            className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+            onClick={() => setReportModal(false)}
           >
             Cancel
           </button>
