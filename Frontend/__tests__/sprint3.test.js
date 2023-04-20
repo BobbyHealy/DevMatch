e291cafcea419bc5a99ecaa626aaf032acc2e686
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-04-20 18:19:11 -0400

    revert Muted Jest test

--- Frontend/__tests__/sprint3.test.js
+++ Frontend/__tests__/sprint3.test.js
@@ -88,108 +88,108 @@ describe("Milestone Test(User Story #6)", () => {
 
 });
 
-// describe("Pin and Unpin Test (User Story #7)", () => {
-//     const pid = "JestProj";
-//     const channel ="main"
-//     test("A DM is default as unpin", async () => {
-//         await setDoc(doc(db, "userChats",senderID ), {});
-//         await setDoc(doc(db, "userChats", receiverID), {});
-//         await sendMsg(DMID, msgID, senderID,receiverID, text)
-//         const queryData = query(doc(db, "chats", DMID, "messages", msgID ));
-//         const msg= await getDoc(queryData);
-//         expect(msg.data().pinned).toBe(false);
+describe("Pin and Unpin Test (User Story #7)", () => {
+    const pid = "JestProj";
+    const channel ="main"
+    test("A DM is default as unpin", async () => {
+        await setDoc(doc(db, "userChats",senderID ), {});
+        await setDoc(doc(db, "userChats", receiverID), {});
+        await sendMsg(DMID, msgID, senderID,receiverID, text)
+        const queryData = query(doc(db, "chats", DMID, "messages", msgID ));
+        const msg= await getDoc(queryData);
+        expect(msg.data().pinned).toBe(false);
 
-//     }, 10000);
-//     test("User can pin a msg in DMs", async () => {
-//         const queryData = query(doc(db, "chats", DMID, "messages", msgID ));
-//         await pinMsg(DMID,msgID,senderID)
-//         const result= await getDoc(queryData);
-//         expect(result.data().pinned).toBe(true);
-//     }, 10000);
-//     test("User can unpin a msg in DMs", async () => {
-//         const msgIDU = "unpinMsg"
+    }, 10000);
+    test("User can pin a msg in DMs", async () => {
+        const queryData = query(doc(db, "chats", DMID, "messages", msgID ));
+        await pinMsg(DMID,msgID,senderID)
+        const result= await getDoc(queryData);
+        expect(result.data().pinned).toBe(true);
+    }, 10000);
+    test("User can unpin a msg in DMs", async () => {
+        const msgIDU = "unpinMsg"
 
-//         // make sure it is pinned before unpin
-//         await sendMsg(DMID, msgIDU, senderID,receiverID, text)
-//         await pinMsg(DMID,msgIDU,senderID)
-//         // unpin the msg
-//         await unpinMsg(DMID,msgIDU)
-//         const queryData = query(doc(db, "chats", DMID, "messages", msgIDU ));
-//         const msg= await getDoc(queryData);
-//         expect(msg.data().pinned).toBe(false);
-//         deleteDoc(doc(db, "userChats",senderID ));
-//         deleteDoc(doc(db, "userChats", receiverID));
-//         deleteDM(DMID)
-//     }, 10000);
-//     test("Msgs in GC are default as unpin", async () => {
-//         createProject(pid)
+        // make sure it is pinned before unpin
+        await sendMsg(DMID, msgIDU, senderID,receiverID, text)
+        await pinMsg(DMID,msgIDU,senderID)
+        // unpin the msg
+        await unpinMsg(DMID,msgIDU)
+        const queryData = query(doc(db, "chats", DMID, "messages", msgIDU ));
+        const msg= await getDoc(queryData);
+        expect(msg.data().pinned).toBe(false);
+        deleteDoc(doc(db, "userChats",senderID ));
+        deleteDoc(doc(db, "userChats", receiverID));
+        deleteDM(DMID)
+    }, 10000);
+    test("Msgs in GC are default as unpin", async () => {
+        createProject(pid)
 
-//         //send a messege in the Jest test GC
-//         await  sendGC(pid,channel,msgID,user,text)
-//         //Fetch the Msg in the GC
-//         const queryData = query(doc(db, "Projects", pid,"TextChannels", channel, "messages", msgID));
-//         const msg= await getDoc(queryData);
-//         //Check if pinned is false 
-//         expect(msg.data().pinned).toBe(false);
-//     }, 10000);
-//     test("User can pin a msg in GC", async () => {
-//         //Fetch the Msg in the GC
-//         const queryData = query(doc(db, "Projects", pid,"TextChannels", channel, "messages", msgID));
-//         //Pin the Msg
-//         await pinGC(pid,channel,msgID, user.userID, user.name)
-//         const result= await getDoc(queryData);
-//         //Check that pinned is true
-//         expect(result.data().pinned).toBe(true);
-//     }, 10000);
-//     test("User can unpin a msg in GC", async () => {
-//         const msgIDU = "unpinMsg"
-//         await  sendGC(pid,channel,msgIDU,user,text)
+        //send a messege in the Jest test GC
+        await  sendGC(pid,channel,msgID,user,text)
+        //Fetch the Msg in the GC
+        const queryData = query(doc(db, "Projects", pid,"TextChannels", channel, "messages", msgID));
+        const msg= await getDoc(queryData);
+        //Check if pinned is false 
+        expect(msg.data().pinned).toBe(false);
+    }, 10000);
+    test("User can pin a msg in GC", async () => {
+        //Fetch the Msg in the GC
+        const queryData = query(doc(db, "Projects", pid,"TextChannels", channel, "messages", msgID));
+        //Pin the Msg
+        await pinGC(pid,channel,msgID, user.userID, user.name)
+        const result= await getDoc(queryData);
+        //Check that pinned is true
+        expect(result.data().pinned).toBe(true);
+    }, 10000);
+    test("User can unpin a msg in GC", async () => {
+        const msgIDU = "unpinMsg"
+        await  sendGC(pid,channel,msgIDU,user,text)
 
-//         // Make sure it is pinned before trying to unpin
-//         await pinGC(pid,channel,msgID, user.userID, user.name)
+        // Make sure it is pinned before trying to unpin
+        await pinGC(pid,channel,msgID, user.userID, user.name)
 
-//         // Unpin the msg
-//         await unpinGC(pid,channel,msgID)
-//         const queryData = query(doc(db, "Projects", pid,"TextChannels", channel, "messages", msgIDU));
-//         const msg= await getDoc(queryData);
-//         // Check the pinned is false
-//         await expect(msg.data().pinned).toBe(false);
-//         deleteProject(pid)
+        // Unpin the msg
+        await unpinGC(pid,channel,msgID)
+        const queryData = query(doc(db, "Projects", pid,"TextChannels", channel, "messages", msgIDU));
+        const msg= await getDoc(queryData);
+        // Check the pinned is false
+        await expect(msg.data().pinned).toBe(false);
+        deleteProject(pid)
         
-//     }, 10000);
-//   });
+    }, 10000);
+  });
 
-//   describe("Voice Channel Test (User Story #21)", () => {
-//     const pid = "US21Test"
-//     const channelVID = "US21"
-//     const vName = "US21"
-//     createProject(pid)
-//     test("User can create voice channels", async () => {
-//         await addVCChannel(pid,channelVID,vName)
-//         const queryData = query(doc(db, "Projects", pid,"VoiceChannels", channelVID));
-//         const vChannel= await getDoc(queryData);
-//         await expect(vChannel).toBeTruthy();
+  describe("Voice Channel Test (User Story #21)", () => {
+    const pid = "US21Test"
+    const channelVID = "US21"
+    const vName = "US21"
+    createProject(pid)
+    test("User can create voice channels", async () => {
+        await addVCChannel(pid,channelVID,vName)
+        const queryData = query(doc(db, "Projects", pid,"VoiceChannels", channelVID));
+        const vChannel= await getDoc(queryData);
+        await expect(vChannel).toBeTruthy();
 
         
 
-//     }, 10000);
-//     test("User can fetch the correct voice channels data", async () => {
-//         const queryData = query(doc(db, "Projects", pid, "VoiceChannels", channelVID));
-//         const vChannel= await getDoc(queryData);
-//         // id is correct
-//         await expect(vChannel.id).toBe(channelVID);
+    }, 10000);
+    test("User can fetch the correct voice channels data", async () => {
+        const queryData = query(doc(db, "Projects", pid, "VoiceChannels", channelVID));
+        const vChannel= await getDoc(queryData);
+        // id is correct
+        await expect(vChannel.id).toBe(channelVID);
 
-//     }, 10000);
+    }, 10000);
 
-//     test("User can delete voice channel", async () => {
-//         await deleteChannel(pid21,channelVID)
-//         const queryData = query(doc(db, "Projects", pid,"VoiceChannels", channelVID));
-//         const vChannel= await getDoc(queryData);
-//         // channel is deleted
-//         await expect(vChannel.exists()).toBe(false)
-//         deleteProject(pid)
-//     }, 10000);
+    test("User can delete voice channel", async () => {
+        await deleteChannel(pid21,channelVID)
+        const queryData = query(doc(db, "Projects", pid,"VoiceChannels", channelVID));
+        const vChannel= await getDoc(queryData);
+        // channel is deleted
+        await expect(vChannel.exists()).toBe(false)
+        deleteProject(pid)
+    }, 10000);
    
-//   });
+  });
 
   
