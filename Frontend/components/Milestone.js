1d365c6c9fbadf0fa6056d5d45862ca3f8027ec5
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-04-20 23:24:25 -0400

    UI restriction

--- Frontend/components/Milestone.js
+++ Frontend/components/Milestone.js
@@ -279,7 +279,7 @@ useEffect(() => {
                             type="button"
                             className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
                         >
-                            Mark all as complete
+                            {project.owners?.includes(user.uid)?"Mark all as complete":"Send Complete Request"}
                         </button>
                         </div>
                     )}
